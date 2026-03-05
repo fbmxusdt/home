8e1cfee4e6ba2dc202ab08e306417c2e35355330
fbmxusdt <fbmxtoken@gmail.com>
2026-03-06 07:54:40 +0800

    Updates

---
+++ assets/all-wallets-CCOiD2DW.js
@@ -0,0 +1,6 @@
+import{w as l}from"./core-D2Rg2SOz.js";import"./index-DoW8df-2.js";import"./index.es-BLlk--eZ.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
